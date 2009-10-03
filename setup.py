bc778b0bd562a6b030c42acb56d12c541b8ef19a
Simon Luijk <simon@luijk.co.uk>
2009-10-03 18:21:57 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,40 @@
+"""
+Based entirely on Django's own ``setup.py``.
+"""
+from distutils.core import setup
+
+# Dynamically calculate the version
+version_tuple = __import__('blog').VERSION
+if version_tuple[2] is not None:
+    version = "%d.%d_%s" % version_tuple
+else:
+    version = "%d.%d" % version_tuple[:2]
+
+setup(
+    name = 'django-blog',
+    description = 'Yet another django blog',
+    version = version,
+    author = 'Simon Luijk',
+    author_email = 'simon@luijk.co.uk',
+    url = 'http://www.apricotwebsolutions.com/blog/',
+    packages = [
+        'blog',
+        'blog.templatetags'
+    ],
+    data_files = [
+        ['blog/templates/feeds', [
+            'blog/templates/feeds/latest_description.html',
+            'blog/templates/feeds/topic_description.html',
+        ]],
+    ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
